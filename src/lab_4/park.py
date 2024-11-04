8ae13bf41224c124cf4cd9f554a896c8f578dcd5
Katia Rudenko <katiarud2007@gmail.com>
2024-11-05 01:06:39 +0200

    Updated using @Property decorators for getters and setters

--- src/lab_4/park.py
+++ src/lab_4/park.py
@@ -8,31 +8,37 @@ class Park:
 
     def __str__(self):
         return (f"Park info:\n"
-                f" (Address: {self.get_address()})\n"
-                f" (Length of bicycle lanes: {self.get_length_of_bicycle_lanes()})\n"
-                f" (Ticket price: {self.get_ticket_price()})\n")
+                f" (Address: {self.address})\n"
+                f" (Length of bicycle lanes: {self.length_of_bicycle_lanes})\n"
+                f" (Ticket price: {self.ticket_price})\n")
 
     def __repr__(self):
-        return (f"address='{self.get_address()}', "
-                f"length_of_bicycle_lanes={self.get_length_of_bicycle_lanes()}, "
-                f"ticket_price={self.get_ticket_price()}, ")
-
-    def get_address(self):
+        return (f"address='{self.address}', "
+                f"length_of_bicycle_lanes={self.length_of_bicycle_lanes}, "
+                f"ticket_price={self.ticket_price}, ")
+    
+    @property
+    def address(self):
         return self.__address
     
-    def get_length_of_bicycle_lanes(self):
+    @property
+    def length_of_bicycle_lanes(self):
         return self.__length_of_bicycle_lanes
     
-    def get_ticket_price(self):
+    @property
+    def ticket_price(self):
         return self.__ticket_price
 
-    def set_address(self, address):
+    @address.setter
+    def address(self, address):
         self.__address = address
     
-    def set_length_of_bicycle_lanes(self, length_of_bicycle_lanes):
+    @length_of_bicycle_lanes.setter
+    def length_of_bicycle_lanes(self, length_of_bicycle_lanes):
         self.__length_of_bicycle_lanes = length_of_bicycle_lanes
     
-    def set_ticket_price(self, ticket_price):
+    @ticket_price.setter
+    def ticket_price(self, ticket_price):
         self.__ticket_price = ticket_price
 
     def __del__(self):
@@ -46,9 +52,9 @@ def main():
     parks = [park_1, park_2, park_3]
 
     max_name_length = max(len(park.name) for park in parks) + 9
-    max_bicycle_lane_length = max(len(str(park.get_length_of_bicycle_lanes())) for park in parks) + 27
-    max_address_length = max(len(park.get_address()) for park in parks) + 8
-    max_ticket_price_length = max(len(str(park.get_ticket_price())) for park in parks) + 17
+    max_bicycle_lane_length = max(len(str(park.length_of_bicycle_lanes)) for park in parks) + 27
+    max_address_length = max(len(park.address) for park in parks) + 8
+    max_ticket_price_length = max(len(str(park.ticket_price)) for park in parks) + 17
     max_visitors_length = max(len(str(park.visitors)) for park in parks) + 17
 
     
@@ -65,9 +71,9 @@ def main():
 
     for park in parks:
         print(f"{park.name:<{max_name_length}} " 
-              f"{park.get_address():<{max_address_length}} " 
-              f"{park.get_length_of_bicycle_lanes():<{max_bicycle_lane_length}} "
-              f"{park.get_ticket_price():<{max_ticket_price_length}} "
+              f"{park.address:<{max_address_length}} " 
+              f"{park.length_of_bicycle_lanes:<{max_bicycle_lane_length}} "
+              f"{park.ticket_price:<{max_ticket_price_length}} "
               f"{park.visitors:<{max_visitors_length}}") 
 
 if __name__ == '__main__':
